970683cfbea90ab194e9e287bef1c3acfbfe4baa
guilherme.deconto <guilherme.deconto@acad.pucrs.br>
2018-03-21 00:31:04 -0300

    Criado as classes coordinates, direction, plateau e main app

---
+++ MarsRover/src/MainApp.java
@@ -0,0 +1,17 @@
+import java util.List;
+
+public class MainApp {
+
+    private Coordinates cCoordinates;
+    private Direction cDirection;
+    private Plateau plateau;
+
+    public MainApp(final Plateau plateau, final Direction direction, final Coordinates coordinates) {
+        this.plateau = plateau;
+        this.cdirection = direction;
+        this.cCoordinates = coordinates;
+    }
+    public void run (final String commandString){
+
+    }
+}
